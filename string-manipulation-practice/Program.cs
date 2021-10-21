415e9d1cfd0d4de6274b2bef512e172003c99152
Thad <thadisfree@gmail.com>
2021-10-20 21:23:32 -0500

    initial commit

---
+++ string-manipulation-practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace string_manipulation_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //watch youtube video on string manipulation
+        }
+    }
+}
