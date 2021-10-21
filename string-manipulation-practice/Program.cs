edadb040e43e7ba05245769eab2f793e39bdbd13
Thad <thadisfree@gmail.com>
2021-10-21 09:42:47 -0500

    practice from youtube

--- string-manipulation-practice/Program.cs
+++ string-manipulation-practice/Program.cs
@@ -8,6 +8,34 @@ namespace string_manipulation_practice
         {
             //watch youtube video on string manipulation
 
+            string text = "  THIS is MY Test Sentence.  ";
+            string modifiedTextLower;
+            string modifiedTextUpper;
+            int modifiedTextLength;
+            string modifiedTextTrim;
+            string modifiedTextSub;
+
+
+            modifiedTextLower = text.ToLower();
+            modifiedTextUpper = text.ToUpper();
+            modifiedTextLength = text.Length;
+            modifiedTextTrim = text.Trim();
+            modifiedTextSub = text.Substring(7);
+
+
+            Console.WriteLine(modifiedTextLower);
+            Console.WriteLine(modifiedTextUpper);
+            Console.WriteLine(modifiedTextTrim);
+            Console.WriteLine(modifiedTextSub);
+            Console.WriteLine(modifiedTextLength);
+
+
+
+            string x = "dkjf;aldkjf;kj:LKJ :LKJ:LH:LKJ:LKJHHH:KH:LKH";
+            int textLength = x.Length;
+            Console.WriteLine(textLength);
+
+
         }
     }
 }
